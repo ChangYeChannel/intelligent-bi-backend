297fb294af323d4755697f3b6653171d1120b642
LiuJian <14878702@qq.com>
2023-04-30 15:19:46 +0800

    -- add 用户相关功能(登录\注册\修改)

--- src/main/java/com/liujian/intelligentbi/config/Knife4jConfig.java
+++ src/main/java/com/liujian/intelligentbi/config/Knife4jConfig.java
@@ -23,12 +23,12 @@ public class Knife4jConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
                         .title("接口文档")
-                        .description("springboot-init")
+                        .description("智能BI平台")
                         .version("1.0")
                         .build())
                 .select()
                 // 指定 Controller 扫描包路径
-                .apis(RequestHandlerSelectors.basePackage("com.liujian.springbootinit.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.liujian.intelligentbi.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
