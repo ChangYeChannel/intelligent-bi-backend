dcd3ac741d8159abb0800487dca1da6a8203b2f4
LiuJian <14878702@qq.com>
2023-04-30 18:09:54 +0800

    -- update 数据库修改上云

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -22,10 +22,14 @@ create table if not exists user
 -- 图表信息表
 create table if not exists chart
 (
-    id         bigint auto_increment comment 'id' primary key,
+    id         bigint          auto_increment          comment 'id' primary key,
+    user_id    bigint                             null comment '用户Id',
     goal       text                               null comment '分析目标',
     chart_data text                               null comment '分析数据',
     chart_type varchar(128)                       null comment '图表类型',
     generate_chart  text                          null comment '生成的图表数据',
-    generate_result text                          null comment '生成的分析结论'
+    generate_result text                          null comment '生成的分析结论',
+    create_time   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete     tinyint      default 0                 not null comment '是否删除'
 ) comment '图表信息' collate = utf8mb4_unicode_ci;
