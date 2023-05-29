63b34c6ab63bb75705c8ab28ea5eb17afda90bc6
ChangYe <15376443491@163.com>
2023-05-29 11:21:30 +0800

    Update create_table.sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE `chart` (
      `user_id` bigint(20) DEFAULT NULL COMMENT '用户Id',
      `chart_name` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '表名',
      `goal` text COLLATE utf8mb4_unicode_ci COMMENT '分析目标',
-     `chart_data` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '分析数据',
+     `chart_data` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '原始数据表名',
      `chart_type` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '图表类型',
      `generate_chart` text COLLATE utf8mb4_unicode_ci COMMENT '生成的图表数据',
      `generate_result` text COLLATE utf8mb4_unicode_ci COMMENT '生成的分析结论',
